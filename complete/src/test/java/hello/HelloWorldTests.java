7a33d57a7ed54d493f7add8fa175184eea85268b
killabayte <killabayte.yarick@gmail.com>
2018-03-20 20:59:03 +0200

    Update HelloWorldTests.java

--- complete/src/test/java/hello/HelloWorldTests.java
+++ complete/src/test/java/hello/HelloWorldTests.java
@@ -31,7 +31,7 @@ public class HelloWorldTests {
     public void shouldPrintHelloWorldToConsole() {
         HelloWorld.main(new String[] { });
 
-        assertThat(output(), containsString("Hello world!"));
+        assertThat(output(), containsString("Hello DevOps World!!!"));
     }
 
     private String output() {
